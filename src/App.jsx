06a0b733f3955bc12e0b3a28c8e790650b42d870
Coel Allen <coelallen@gmail.com>
2023-01-20 10:55:48 -0700

    hardcoded card

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import "./App.css";
 import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import Card from "./components/Card.jsx";
+import data from "./data.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  const cards = data.map((d) => {});
+  return (
+    <div className="App">
+      <Navbar />
+      <Card />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="nav">
+      <h3 className="nav--text">my travel journal.</h3>
+    </div>
+  );
+}
